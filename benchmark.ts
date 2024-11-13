3471bcfee2f16d8b09988e89309efd9a9ee1e5cd
Yoeven D Khemlani <yoevenkhem@gmail.com>
2024-11-13 22:40:09 +0000

    update

--- benchmark.ts
+++ benchmark.ts
@@ -1,3 +1,5 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import fs from "fs";
 import Groq from "groq-sdk";
 import { AssemblyAI } from "assemblyai";
@@ -5,18 +7,19 @@ import { JigsawStack } from "jigsawstack";
 
 const audioUrl =
   "https://rogilvkqloanxtvjfrkm.supabase.co/storage/v1/object/public/demo/Video%201737458382653833217.mp4"; // set your audio URL here
-const aai = new AssemblyAI({ apiKey: "" });
+
+const aai = new AssemblyAI({ apiKey: process.env.ASSEMBLYAI_API_KEY! });
 const groq = new Groq({
-  apiKey: "",
+  apiKey: process.env.GROQ_API_KEY!,
 });
 const jigsawstack = JigsawStack({
-  apiKey: "",
+  apiKey: process.env.JIGSAWSTACK_API_KEY!,
 });
 
-async function runGroq() {
+const runGroq = async () => {
   console.time("STT_Groq");
   try {
-    const transcription = await groq.audio.transcriptions.create({
+    await groq.audio.transcriptions.create({
       file: fs.createReadStream("sample.mp4"),
       model: "whisper-large-v3-turbo",
       language: "en",
@@ -26,36 +29,49 @@ async function runGroq() {
     console.error("Error in Groq:", error);
   }
   console.timeEnd("STT_Groq");
-}
+};
 
-async function runJigsawStack() {
+const runJigsawStack = async () => {
   console.time("STT_JigsawStack");
   try {
-    const transcription = await jigsawstack.audio.speech_to_text({
+    await jigsawstack.audio.speech_to_text({
       url: audioUrl,
     });
   } catch (error) {
     console.error("Error in JigsawStack:", error);
   }
   console.timeEnd("STT_JigsawStack");
-}
+};
 
 const runAssemblyAI = async () => {
   console.time("STT_AssemblyAI");
   try {
-    const transcript = await aai.transcripts.transcribe({
+    await aai.transcripts.transcribe({
       audio_url: audioUrl,
     });
-    // console.log(transcript.text);
   } catch (error) {
     console.error("Error in AssemblyAI:", error);
   }
   console.timeEnd("STT_AssemblyAI");
 };
 
+const avgResponseTime = (times: number[]) =>
+  times.reduce((a, b) => a + b, 0) / times.length;
+
+const sst = async (func: () => Promise<void>) => {
+  const start = performance.now();
+  await func();
+  const end = performance.now();
+  return end - start;
+};
+
 const benchmark = async () => {
   const iterations = 10;
-  const responseTimes = {
+  const responseTimes: {
+    Groq: number[];
+    JigsawStack: number[];
+    AssemblyAI: number[];
+  } = {
     Groq: [],
     JigsawStack: [],
     AssemblyAI: [],
@@ -65,26 +81,16 @@ const benchmark = async () => {
     console.log(`Iteration ${i + 1}`);
 
     // Measure Groq response time
-    const groqStart = Date.now();
-    await runGroq();
-    const groqEnd = Date.now();
-    responseTimes.Groq.push(groqEnd - groqStart);
+    const [groqTime, JigsawStackTime, assemblyTime] = await Promise.all([
+      sst(runGroq),
+      sst(runJigsawStack),
+      sst(runAssemblyAI),
+    ]);
 
-    // Measure JigsawStack response time
-    const jigsawStart = Date.now();
-    await runJigsawStack();
-    const jigsawEnd = Date.now();
-    responseTimes.JigsawStack.push(jigsawEnd - jigsawStart);
-
-    // Measure AssemblyAI response time
-    const assemblyStart = Date.now();
-    await runAssemblyAI();
-    const assemblyEnd = Date.now();
-    responseTimes.AssemblyAI.push(assemblyEnd - assemblyStart);
+    responseTimes.Groq.push(groqTime);
+    responseTimes.JigsawStack.push(JigsawStackTime);
+    responseTimes.AssemblyAI.push(assemblyTime);
   }
-
-  const avgResponseTime = (times) =>
-    times.reduce((a, b) => a + b, 0) / times.length;
 
   console.log(
     `Average response time for Groq: ${avgResponseTime(responseTimes.Groq)} ms`
