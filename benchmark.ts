f1a284c0edb81f188d839b60c0d0cded16a54d08
Yoeven D Khemlani <yoevenkhem@gmail.com>
2024-11-21 19:41:16 +0100

    Update benchmark.ts

--- benchmark.ts
+++ benchmark.ts
@@ -7,16 +7,16 @@ import { JigsawStack } from "jigsawstack";
 
 const audioSamples = [
   {
+    name: "sample3.mp3",
+    url: "https://uuvhpoxkzjnrvvajhnyb.supabase.co/storage/v1/object/sign/default/preview/stt-examples/stt_long_audio_sample_3.mp3?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJkZWZhdWx0L3ByZXZpZXcvc3R0LWV4YW1wbGVzL3N0dF9sb25nX2F1ZGlvX3NhbXBsZV8zLm1wMyIsImlhdCI6MTczMjIwMjM0NywiZXhwIjozMTU1MzAwNjY2MzQ3fQ.KX1NxaFIfVRdWFsp1sToQNreRLzWIThJHYGyR6xcGbs&t=2024-11-21T15%3A19%3A07.592Z",
+  },
+  {
     name: "sample1.mp4",
     url: "https://uuvhpoxkzjnrvvajhnyb.supabase.co/storage/v1/object/sign/default/preview/stt-examples/stt_short_audio_sample_1.mp4?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJkZWZhdWx0L3ByZXZpZXcvc3R0LWV4YW1wbGVzL3N0dF9zaG9ydF9hdWRpb19zYW1wbGVfMS5tcDQiLCJpYXQiOjE3MzIyMDIzMDYsImV4cCI6MzE1NTMwMDY2NjMwNn0.I0-T5I-nkdeykmizHmq1Ut_Jjs66DxzApv2XbrV9MyM&t=2024-11-21T15%3A18%3A26.030Z",
   },
   {
     name: "sample2.mp3",
     url: "https://uuvhpoxkzjnrvvajhnyb.supabase.co/storage/v1/object/sign/default/preview/stt-examples/stt_very_short_audio_sample_2.mp3?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJkZWZhdWx0L3ByZXZpZXcvc3R0LWV4YW1wbGVzL3N0dF92ZXJ5X3Nob3J0X2F1ZGlvX3NhbXBsZV8yLm1wMyIsImlhdCI6MTczMjIwMzIwNywiZXhwIjozMTU1MzAwNjY3MjA3fQ._R0cLbrIx_FUR3CMRYaUMj616diA_1fjWUcVq2vAONg&t=2024-11-21T15%3A33%3A27.154Z",
-  },
-  {
-    name: "sample3.mp3",
-    url: "https://uuvhpoxkzjnrvvajhnyb.supabase.co/storage/v1/object/sign/default/preview/stt-examples/stt_long_audio_sample_3.mp3?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJkZWZhdWx0L3ByZXZpZXcvc3R0LWV4YW1wbGVzL3N0dF9sb25nX2F1ZGlvX3NhbXBsZV8zLm1wMyIsImlhdCI6MTczMjIwMjM0NywiZXhwIjozMTU1MzAwNjY2MzQ3fQ.KX1NxaFIfVRdWFsp1sToQNreRLzWIThJHYGyR6xcGbs&t=2024-11-21T15%3A19%3A07.592Z",
   },
 ];
 
@@ -28,8 +28,8 @@ const jigsawstack = JigsawStack({
   apiKey: process.env.JIGSAWSTACK_API_KEY!,
 });
 
-const runGroq = async (audioUrl: string) => {
-  await groq.audio.transcriptions.create({
+const runGroq = (audioUrl: string) => {
+  return groq.audio.transcriptions.create({
     file: fs.createReadStream(audioUrl),
     model: "whisper-large-v3-turbo",
     language: "en",
@@ -37,14 +37,14 @@ const runGroq = async (audioUrl: string) => {
   });
 };
 
-const runJigsawStack = async (audioUrl: string) => {
-  await jigsawstack.audio.speech_to_text({
+const runJigsawStack = (audioUrl: string) => {
+  return jigsawstack.audio.speech_to_text({
     url: audioUrl,
   });
 };
 
-const runAssemblyAI = async (audioUrl: string) => {
-  await aai.transcripts.transcribe({
+const runAssemblyAI = (audioUrl: string) => {
+  return aai.transcripts.transcribe({
     audio_url: audioUrl,
   });
 };
@@ -54,10 +54,12 @@ const stt = async (
   audioUrl: string
 ) => {
   const start = performance.now();
-  await func(audioUrl).catch((error) => {
+  try {
+    await func(audioUrl);
+  } catch (error) {
     console.error(`Error in ${func.name}:`, error);
     return -1;
-  });
+  }
   const end = performance.now();
   const time = end - start;
   console.log(`${func.name} took ${time}ms or ${time / 1000}s`);
@@ -112,7 +114,7 @@ const benchmark = async () => {
   ) {
     const audioURL = audioSamples[audioSampleIndex].url;
 
-    const sampleKey = `sample_${audioSampleIndex + 1}`;
+    const sampleKey = audioSamples[audioSampleIndex].name;
 
     responseTimes.samples[sampleKey] = {
       url: audioURL,
